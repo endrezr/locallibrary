ec9e78e88890cab1d72dbb1bd5bc34b51e7b8bbb
endre <endrezr@gmail.com>
2017-03-07 20:41:01 +0100

    sceleton app

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '47ot7a@k=-4j28kayr0i0e9eq3_3a#j3e&_iojqdzn_35(92r7'
+SECRET_KEY ='1500bre9956414' #'47ot7a@k=-4j28kayr0i0e9eq3_3a#j3e&_iojqdzn_35(92r7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,3 +19,23 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+#using inlude to add urls
+from django.conf.urls import include
+
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+
+#Add url maps to redirect the base url to our Application
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+#use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
